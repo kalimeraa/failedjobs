cdc9bf77593267eb0b6499eb7e9e8d515df6d8e1
bivan1 <hi.mrttpz@gmail.com>
2020-03-15 14:27:29 +0300

    feature deleted database process

--- app/Http/Controllers/FailedJobController.php
+++ app/Http/Controllers/FailedJobController.php
@@ -15,27 +15,12 @@ class FailedJobController extends Controller
             'exception' => 'asd'
         ];
 
-        // DB::table('failed_jobs')->insert($failedJobData);
-
         $jsonPayload = json_decode($failedJobData['payload']);
         if (empty($jsonPayload) || !property_exists($jsonPayload,'data') || !property_exists($jsonPayload->data,'command')) {
             dd('please paste your payload of the failed_jobs');
         }
 
         $data = unserialize($jsonPayload->data->command);
-        /**
-         * // Closure::bind() actually creates a new instance of the closure
-         * $sweetsThief = function ($kitchen) {
-         * return $kitchen->chained;
-         * };
-         *
-         * $newData = new $data;
-         *
-         * $sweetsThief = Closure::bind($sweetsThief, null, $newData);
-         * dd($sweetsThief);
-         * var_dump($sweetsThief($newData));
-         */
-        // dd($this->getProperty($data,'chained'));
         if ($request->post('exportType') === 'dd') {
             dd($data);
         } elseif ($request->post('exportType') === 'print_r') {
